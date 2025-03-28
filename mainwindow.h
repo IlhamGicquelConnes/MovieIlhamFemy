fad11409aa858b3e69ddfead1ccf1d6d29d9b184
femy-dg <femy.domingo@stfelixlasalle.fr>
2025-03-28 17:32:25 +0100

    Add files via upload

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtSql/QtSql>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,6 +15,24 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private:
+    void rechercher();
+    void afficherResultat( QSqlQuery query );
+    void ajouterConditionAnnnee( QString & requete);
+    void ajouterConditionRealisateur( QString & requete);
+    void ajouterConditionTitre( QString & requete);
+
+private slots:
+    void on_pushButtonRechercher_clicked();
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_reponseTable_cellClicked(int row, int column);
+
+    void on_lineEdit_cursorPositionChanged(int arg1, int arg2);
 
 private:
     Ui::MainWindow *ui;
